2fc20c015a1c0ee8f0912018827f20799a1ceacd
mperezf <miguelperezfernandez31@gmail.com>
2018-12-30 18:48:47 +0100

    Initial commit

---
+++ app/controllers/RecipeController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import play.mvc.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class RecipeController extends Controller {
+
+
+    public Result index() {
+        return Results.ok();
+    }
+
+
+}
